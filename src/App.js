09bbf06c114edba1d72fa7193144ad41df9832de
ferdysan360 <ferdysantoso@icloud.com>
2021-08-28 15:04:06 +0700

    Add few UI components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { jsx, css } from '@emotion/react'
 import { useState } from 'react';
 import PokemonList from './Containers/PokemonList';
+import PokedexLogo from './Assets/pokedex_logo.png';
 
 function App() {
 
@@ -12,19 +13,48 @@ function App() {
     margin: 10px 0px;
   `
 
+  const section = css`
+    margin: 20px 40px;
+    max-width: 1440px;
+  `
+
+  const flex = css`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+  `
+
+  const pokedexLogoStyle = css`
+    max-height: 50px;
+  `
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <button css={navMenuStyle} onClick={() => { setPage("pokemonList") }}>Pokemon List</button>
-        <button css={navMenuStyle} onClick={() => { setPage("myPokemon") }}>My Pokemon</button>
+        <div css={section}>
+          <img css={pokedexLogoStyle} src={PokedexLogo} alt="pokedex-logo" />
+          <div>
+            <button css={navMenuStyle} onClick={() => { setPage("pokemonList") }}>Pokemon List</button>
+            <button css={navMenuStyle} onClick={() => { setPage("myPokemon") }}>My Pokemon</button>
+          </div>
+        </div>
       </header>
-      {
-        page === "pokemonList" ? 
-          <PokemonList />
-        : page === "myPokemon" ?
-          <div>My Pokemon</div>
-        : <div>Pokemon Details</div>
-      }
+      <div css={flex}>
+        <div css={section}>
+          {
+            page === "pokemonList" ? 
+            <PokemonList />
+            : page === "myPokemon" ?
+            <h1>My Pokemon</h1>
+            : page === "pokemonDetails" ?
+            <h1>Pokemon Details</h1>
+            : <h1>Page Not Found</h1>
+          }
+        </div>
+      </div>
     </div>
   );
 }
--- src/Containers/PokemonList.js
+++ src/Containers/PokemonList.js
@@ -1,3 +1,4 @@
+/** @jsxImportSource @emotion/react */
 import { jsx, css } from '@emotion/react'
 import { useEffect, useState } from 'react';
 import axios from 'axios';
@@ -23,22 +24,36 @@ function PokemonList() {
         })
     }, [url]);
 
+    const pokemonButtonStyle = css`
+        margin: 10px;
+        padding: 10px;
+        border: 1px solid black;
+        border-radius: 8px;
+        background-color: white;
+    `
+
+    const PokemonButton = ({ pokemon }) => (
+        <a 
+            href={pokemon.url} 
+            target="_blank" 
+            rel="noopener noreferrer"
+        >
+            <button css={pokemonButtonStyle}>
+                {pokemon.name}
+            </button>
+        </a>
+    )
+
     return (
         <div>
-            <div>Pokemon List</div>
+            <h1>Pokemon List</h1>
             {pokemonList?.map((pokemon) => (
-                <div>
-                    <a 
-                        href={pokemon.url} 
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                    >
-                        {pokemon.name}
-                    </a>
-                </div>
+                <PokemonButton pokemon={pokemon}/>
             ))}
-            <button disabled={prev === null} onClick={() => { setUrl(prev) }}>Prev</button>
-            <button disabled={next === null} onClick={() => { setUrl(next) }}>Next</button>
+            <div>
+                <button disabled={prev === null} onClick={() => { setUrl(prev) }}>Prev</button>
+                <button disabled={next === null} onClick={() => { setUrl(next) }}>Next</button>
+            </div>
         </div>
         
     );
