4490f4b114016584f9bef27b4a30a979b9a23616
ferdysan360 <ferdysantoso@icloud.com>
2021-08-31 21:16:26 +0700

    Change PokemonCard Button Style

--- src/Components/PokemonCard.js
+++ src/Components/PokemonCard.js
@@ -1,6 +1,9 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import { useWindowDimensions } from '../Utils/Utility';
+import Button from '@material-ui/core/Button';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { indigo, red } from '@material-ui/core/colors';
 
 const PokemonCard = ({ pokemon, countFunction, detailsFunction, releaseFunction, index }) => {
 
@@ -18,14 +21,13 @@ const PokemonCard = ({ pokemon, countFunction, detailsFunction, releaseFunction,
         background-color: white;
         box-shadow: 5px 10px 18px #888888;
         font-size: 20px;
+        word-break: break-all;
     `
 
     const cardResponsiveStyle = 
     width < 520 ? 
         (css`
-            min-width: 120px;
-            min-height: 75px;
-            font-size: 16px;
+            width: 100%;
         `) 
     : 
         (css`
@@ -37,20 +39,60 @@ const PokemonCard = ({ pokemon, countFunction, detailsFunction, releaseFunction,
         margin: 5px 0px;
     `
 
-    const detailsButtonStyle = css`
-        margin: 3px 0px;
+    const pokemonNicknameStyle = css`
+        color: #828282;
+        font-size: 24px;
+        font-weight: 700;
     `
+
+    const pokemonNameStyle = css`
+        margin: 5px 0px;
+    `
+
+    const BlueButton = withStyles((theme) => ({
+        root: {
+            'border-color': indigo[700],
+            '&:hover': {
+                color: indigo[50],
+                backgroundColor: indigo[900],
+            },
+        },
+    }))(Button);
+
+    const RedButton = withStyles((theme) => ({
+        root: {
+            color: theme.palette.getContrastText(red[600]),
+            backgroundColor: red[600],
+            '&:hover': {
+                backgroundColor: red[900],
+            },
+        },
+    }))(Button);
+
+    const useStyles = makeStyles((theme) => ({
+        margin: {
+            margin: theme.spacing(0.5),
+        },
+    }));
+
+    const classes = useStyles();
 
     return (
         <div css={[pokemonButtonStyle, cardResponsiveStyle]}>
             {
                 pokemon.nickname ?
-                    (<div>
+                    (<div css={pokemonNicknameStyle}>
                         {pokemon.nickname}
                     </div>)
                 : (<></>)
             }
-            {pokemon.name}
+            {
+                pokemon.name ?
+                    (<div css={pokemonNameStyle}>
+                        {pokemon.name}
+                    </div>)
+                : (<></>)
+            }
             {
                 countFunction ?
                     (<div css={countStyle}>
@@ -60,16 +102,16 @@ const PokemonCard = ({ pokemon, countFunction, detailsFunction, releaseFunction,
             }
             {
                 detailsFunction ?
-                    <button css={detailsButtonStyle} onClick={() => { detailsFunction(pokemon.name) }}>
+                    <BlueButton size="small" variant="outlined" color="primary" className={classes.margin} onClick={() => { detailsFunction(pokemon.name) }} >
                         Details
-                    </button>
+                    </BlueButton>
                 : (<></>)
             }
             {
                 releaseFunction ?
-                    <button css={detailsButtonStyle} onClick={() => { releaseFunction(pokemon.name, index) }}>
+                    <RedButton size="small" variant="contained" color="primary" className={classes.margin} onClick={() => { releaseFunction(pokemon.name, index) }} >
                         Release
-                    </button>
+                    </RedButton>
                 : (<></>)
             }
         </div>
--- src/Containers/MyPokemon.js
+++ src/Containers/MyPokemon.js
@@ -29,45 +29,6 @@ const MyPokemon = ({ myPokemonCallback }) => {
         margin: 0px 0px 20px 0px;
     `
 
-    const inlineDiv = css`
-        display: inline-block;
-        margin: 10px;
-    `
-
-    const pokemonButtonStyle = css`
-        padding: 10px;
-        border-radius: 8px;
-        background-color: white;
-        font-size: 20px;
-        color: #DC0A2D;
-        font-weight: 700;
-        cursor: pointer;
-        :hover {
-            background-color: #E6E6E6;
-        }
-        z-index: 1;
-    `
-
-    const releaseButtonStyle = css`
-        padding: 5px;
-        border-radius: 8px;
-        background-color: #DC0A2D;
-        font-size: 16px;
-        color: black;
-        font-weight: 700;
-        cursor: pointer;
-        :hover {
-            background-color: #FF0000;
-        }
-        z-index: 0;
-    `
-
-    const pokemonNameStyle = css`
-        font-size: 14px;
-        color: black;
-        font-weight: 500;
-    `
-
     const Alert = (props) => {
         return <MuiAlert elevation={6} variant="filled" {...props} />;
     }
@@ -90,30 +51,6 @@ const MyPokemon = ({ myPokemonCallback }) => {
         }
     }
 
-    const ReleaseButton = ({ pokemon, index }) => (
-        <button 
-            css={releaseButtonStyle}
-            onClick={() => { releasePokemon(pokemon, index) }}
-        >
-            Release
-        </button>
-    )
-
-    const PokemonButton = ({ pokemon }) => (
-        <button
-            css={pokemonButtonStyle}
-            onClick={() => { callback(pokemon.name) }}
-        >
-            <div>
-                {pokemon.nickname}
-            </div>
-            <div css={pokemonNameStyle}>
-                {pokemon.name}
-            </div>
-        </button>
-    )
-
-
     return (
         <div>
             <h1>My Pokemon</h1>
@@ -123,15 +60,7 @@ const MyPokemon = ({ myPokemonCallback }) => {
                 :
                 <div css={flexPokemonList}>
                     {pokemonList?.map((pokemon, index) => (
-                        <div>
-                            {/* <div>
-                                <PokemonButton pokemon={pokemon} index={index} />
-                            </div>
-                            <div>
-                                <ReleaseButton pokemon={pokemon} index={index} />
-                            </div> */}
-                            <PokemonCard pokemon={pokemon} detailsFunction={detailsPokemon} releaseFunction={releasePokemon} index={index} />
-                        </div>
+                        <PokemonCard pokemon={pokemon} detailsFunction={detailsPokemon} releaseFunction={releasePokemon} index={index} />
                     ))}
                 </div>
             }
